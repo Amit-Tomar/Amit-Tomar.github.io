9a9a5f2923ed796f287e151c885f4c9a76646848
Amit Tomar <amittomar@ck12admins-MacBook-Air.local>
2020-01-20 09:22:50 +0530

    Added RX decoder challenge init

---
+++ IIITB-RX/precache-manifest.59a1109785d277af5cf4bec5ef26730b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60caca766bd1b4e3d3577e5ab3e29f33",
+    "url": "/IIITB-RX/index.html"
+  },
+  {
+    "revision": "79b6cb6c1c1cb051e51c",
+    "url": "/IIITB-RX/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b1b1981cd145b8d5e8a1",
+    "url": "/IIITB-RX/static/js/2.7f8cd285.chunk.js"
+  },
+  {
+    "revision": "0d2cefbfe8b69355ff9258cf8fa89d51",
+    "url": "/IIITB-RX/static/js/2.7f8cd285.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79b6cb6c1c1cb051e51c",
+    "url": "/IIITB-RX/static/js/main.e6147d0b.chunk.js"
+  },
+  {
+    "revision": "08383989b9523cff7f9f",
+    "url": "/IIITB-RX/static/js/runtime-main.9b2a6f7a.js"
+  }
+]);
--- IIITB-RX/service-worker.js
+++ IIITB-RX/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64e614d8c9b1d0242dc453a8dfd0de2a.js"
+  "/IIITB-RX/precache-manifest.59a1109785d277af5cf4bec5ef26730b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IIITB-RX/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

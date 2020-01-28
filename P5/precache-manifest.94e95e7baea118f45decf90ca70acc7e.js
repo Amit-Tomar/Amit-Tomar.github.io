1bac89aa8bd3d51f0885b3169b7913108d5bf84e
Amit Tomar <amittomar@ck12admins-MacBook-Air.local>
2020-01-29 00:01:15 +0530

    Added P5

---
+++ P5/precache-manifest.94e95e7baea118f45decf90ca70acc7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b25427300356a2379cf88c9ce5b99e",
+    "url": "/P5/index.html"
+  },
+  {
+    "revision": "7d8ae0191a531167b9f0",
+    "url": "/P5/static/css/main.2fc71851.chunk.css"
+  },
+  {
+    "revision": "ee14339ec456fa59aaa9",
+    "url": "/P5/static/js/2.dc805839.chunk.js"
+  },
+  {
+    "revision": "86c5b350d6e7dd3fd3bea848754c5b02",
+    "url": "/P5/static/js/2.dc805839.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d8ae0191a531167b9f0",
+    "url": "/P5/static/js/main.2bc32ca9.chunk.js"
+  },
+  {
+    "revision": "2b02cd6544614adf0ae7",
+    "url": "/P5/static/js/runtime-main.d1643531.js"
+  }
+]);

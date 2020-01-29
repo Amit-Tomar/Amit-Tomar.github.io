b2e29b9d8432c85e84a37a45e73ddc1691f598ea
Amit-Tomar <amit.tomar@iiitb.org>
2020-01-29 11:08:42 +0530

    Updated routing

---
+++ P5/precache-manifest.1154327ce713b88c4ad0b35017f30893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfea3c4dfda876d12d909ee37e78903f",
+    "url": "/P5/index.html"
+  },
+  {
+    "revision": "c81852c39c999910ccfd",
+    "url": "/P5/static/css/main.2fc71851.chunk.css"
+  },
+  {
+    "revision": "ee14339ec456fa59aaa9",
+    "url": "/P5/static/js/2.dc805839.chunk.js"
+  },
+  {
+    "revision": "86c5b350d6e7dd3fd3bea848754c5b02",
+    "url": "/P5/static/js/2.dc805839.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c81852c39c999910ccfd",
+    "url": "/P5/static/js/main.4067acaf.chunk.js"
+  },
+  {
+    "revision": "2b02cd6544614adf0ae7",
+    "url": "/P5/static/js/runtime-main.d1643531.js"
+  }
+]);
